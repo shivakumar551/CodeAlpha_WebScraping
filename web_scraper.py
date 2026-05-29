e810c480201fc76c2121f4df922f790cd6e66f63
shivakumar551 <shivakumarsailla@gmail.com>
2026-05-29 11:27:12 +0530

    Update web_scraper.py

--- web_scraper.py
+++ web_scraper.py
@@ -1,23 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-
-# Website URL
 url = "http://quotes.toscrape.com"
-
-# Send request
 response = requests.get(url)
-
-# Parse HTML
 soup = BeautifulSoup(response.text, "html.parser")
-
-# Find all quotes
 quotes = soup.find_all("div", class_="quote")
-
-# Empty list
 data = []
-
-# Extract quote and author
 for quote in quotes:
     text = quote.find("span", class_="text").text
     author = quote.find("small", class_="author").text
